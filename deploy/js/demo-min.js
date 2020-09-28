4daab501546b9148cf8eae613f21d870392469e2
js4679 <63320565+js4679@users.noreply.github.com>
2020-09-27 20:52:02 -0400

    no show

--- deploy/js/demo-min.js
+++ deploy/js/demo-min.js
@@ -1,1 +1,1 @@
-import{gsap}from"gsap";gsap.to(".red-box",{duration:2,alpha:0});
+import{gsap}from"gsap";gsap.to(".red-box",{duration:2,alpha:0}),gsap.to(".blue-box",{duration:4,alpha:0});
--- dev/js/demo.js
+++ dev/js/demo.js
@@ -2,5 +2,7 @@ import {gsap} from "gsap";
 
 gsap.to(".red-box",{duration: 2, alpha:0});
 
+gsap.to(".blue-box",{duration: 4, alpha:0});
+
 
 
